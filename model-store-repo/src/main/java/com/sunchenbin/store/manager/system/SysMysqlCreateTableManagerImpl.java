b72136849a330fa5647e0bbc4412637b5b45afc4
chenbin.sun <chenbin.sun@baozun.cn>
2016-06-24 17:07:05 +0800

    追加配置

--- model-store-repo/src/main/java/com/sunchenbin/store/manager/system/SysMysqlCreateTableManagerImpl.java
+++ model-store-repo/src/main/java/com/sunchenbin/store/manager/system/SysMysqlCreateTableManagerImpl.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.Validator;
 
 import com.sunchenbin.store.annotation.Column;
 import com.sunchenbin.store.annotation.LengthCount;
